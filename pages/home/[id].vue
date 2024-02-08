94f66b6afcfd402409f80c2a1dc9236b29c428cc
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-08 21:17:12 +0700

    feat: featuring home pages

---
+++ pages/home/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-3xl">
+    Test
+    {{ route }}
+  </h1>
+</template>
+
+<script lang="ts" setup>
+  const route = useRoute().params
+</script>
+
+<style>
+
+</style>
