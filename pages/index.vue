a0dcde377a8c0c53f5f7518ee474bf84fe2d6c79
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-08 21:16:33 +0700

    feat(featuring login pages): build login pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,31 @@
 <template>
-  <header>
-    <Navbar />
-  </header>
-  <main>
-    <section>
-      <div class="bg-[#f9edea] w-full px-10 py-10">
-        <div class="grid grid-cols-3 gap-5 auto-rows-[250px]">
-          <div class="col-span-2 row-span-2">
-            <img
-              src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="image1"
-              class="w-full h-full object-cover rounded-2xl"
-            />
-          </div>
-          <div class="col-span-1">
-            <img
-              src="https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="image2"
-              class="w-full h-full object-cover rounded-2xl"
-            />
-          </div>
-          <div class="col-span-1">
-            <img
-              src="https://images.unsplash.com/photo-1584917865442-de89df76afd3?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="img3"
-              class="w-full h-full object-cover rounded-2xl"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+  <div class="container flex flex-row min-h-screen items-center justify-center">
+    <div>
+      <h1>Welcome</h1>
+      <p>Sign in to the Meraki Shop</p>
+      <button
+        @click="signIn()"
+        class="px-3 py-2 rounded-md bg-emerald-500 text-white inline-flex items-center justify-center"
+      >
+        Sign in
+      </button>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const userStore = useUserStore();
+
+definePageMeta({
+  middleware: ["already-auth"],
+});
+
+const signIn = async () => {
+  await userStore.signIn({
+    username: "kminchelle",
+    password: "0lelplR",
+  });
+
+  await navigateTo('/home', { replace: true })
+};
+</script>
