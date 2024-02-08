5d7a90bb243ea584fe939403566cd54ae6468b20
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-08 22:40:16 +0700

    feat: adding vitest modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-vitest"],
   app: {
     head :{
       charset: "utf-8",
@@ -14,5 +14,6 @@ export default defineNuxtConfig({
   },
   imports: {
     dirs: ["./stores"]
-  }
+  },
+  sourcemap: false,
 })
