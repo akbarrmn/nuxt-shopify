8f340bee6f196af58c4bf65a3a9168eed364a632
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-08 21:14:52 +0700

    style: configure tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,16 @@ export default <Partial<Config>> {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          default: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        }
+      }
     }
   },
   plugins: []
