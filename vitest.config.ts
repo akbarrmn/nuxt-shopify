1a705e444a031a7677136d87eee2387035218090
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-09 21:14:59 +0700

    feat: configuring vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,12 @@
-import { defineVitestConfig } from 'nuxt-vitest/config'
+import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
-  // any custom vitest config you require
+    test:{
+        globals: true,
+        coverage: {
+            all: false,
+        },
+        environment: "happy-dom",
+        
+    },
 })
