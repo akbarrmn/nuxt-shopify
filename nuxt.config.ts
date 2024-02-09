e120e1164931102ab669ff75b97c224588a13f2f
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-09 21:14:32 +0700

    feat: adding testing library called vitest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-vitest"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxt/test-utils/module"],
   app: {
     head :{
       charset: "utf-8",
